7377055be35ed55be4c9735d2683c02ff7b9dc0f
Paidipelly Dhruvateja <81409709+P-Dhruva-Teja@users.noreply.github.com>
2023-02-20 19:57:46 +0530

    Create Arithmetic_Operators.c

---
+++ Operators/Programs on operators/Arithmetic_Operators.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter three numbers :");
+  scanf("%d %d %d", &a,&b,&c);
+  int add = a+b+c;            //addition
+  int sub = a-b-c;            //subtraction
+  int mul = a*b*c;            //multiplication
+  i divi = a/b;             //division
+  int mod = a%b;              //factorial division
+  printf("Addition :%d\n",add);
+  printf("subtraction :%d\n",sub);
+  printf("Multiplication :%d\n",mul);
+  printf("Division :%d\n",divi);
+  printf("Factorial division :%d\n",mod);
+  /*Now lets observe their precedence*/
+  
+  
+  
+  
+}
